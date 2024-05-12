1f6ffcc8bfc607a9b59390e3c55d01db99bd8587
TotallyKeithYes <135842382+GopherDevGames@users.noreply.github.com>
2024-05-13 00:57:15 +0200

    reworked temp pixel system, working water

--- main.cpp
+++ main.cpp
@@ -20,28 +20,27 @@ const uint8_t gases[P_TYPES_AMOUNT] = {true,false,false};
 const uint8_t liquids[P_TYPES_AMOUNT] = {true,false,true};
 const uint8_t solids[P_TYPES_AMOUNT] = {false,true,false};
 
-uint8_t temp_pixels[P_COUNT];
 uint8_t pixels[P_COUNT];
+bool placed_mask[P_COUNT];
 
 uint8_t get_pixel(uint16_t x,uint16_t y){
     return pixels[(x*WINDOW_DEFAULT_H)+y];
 }
-uint8_t get_temp_pixel(uint16_t x,uint16_t y){
-    return temp_pixels[(x*WINDOW_DEFAULT_H)+y];
-}
-void set_temp_pixel(uint16_t x,uint16_t y,uint8_t pixel){
-    temp_pixels[(x*WINDOW_DEFAULT_H)+y] = pixel;
-}
 
-void set_pixel(uint16_t x,uint16_t y,uint8_t pixel){
+void set_pixel(uint16_t x,uint16_t y,uint8_t pixel, bool set_placed_mask = true){
     pixels[(x*WINDOW_DEFAULT_H)+y] = pixel;
+    placed_mask[(x*WINDOW_DEFAULT_H)+y] = set_placed_mask;
 }
 
-#define P_DOWN get_temp_pixel(x, static_cast<uint16_t>(std::max(y-1,1)))
-#define P_LEFT get_temp_pixel(static_cast<uint16_t>(std::max(x-1,1)), y)
-#define P_RIGHT get_temp_pixel(static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), y)
-#define P_DOWN_LEFT get_temp_pixel(static_cast<uint16_t>(std::max(x-1,1)), static_cast<uint16_t>(std::max(y-1,1)))
-#define P_DOWN_RIGHT get_temp_pixel(static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), static_cast<uint16_t>(std::max(y-1,1)))
+bool get_placed_mask(uint16_t x,uint16_t y){
+    return placed_mask[(x*WINDOW_DEFAULT_H)+y];
+}
+
+#define P_DOWN x, static_cast<uint16_t>(std::max(y-1,1))
+#define P_LEFT static_cast<uint16_t>(std::max(x-1,1)), y
+#define P_RIGHT static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), y
+#define P_DOWN_LEFT static_cast<uint16_t>(std::max(x-1,1)), static_cast<uint16_t>(std::max(y-1,1))
+#define P_DOWN_RIGHT static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), static_cast<uint16_t>(std::max(y-1,1))
 
 // GLobals
 bool is_drawing = false;
@@ -62,50 +61,50 @@ void run_logic(){
         mouse_x/=size_multiplier;
         uint16_t pos_x = std::min(static_cast<uint16_t>(mouse_x),WINDOW_DEFAULT_W);
         uint16_t pos_y = std::min(static_cast<uint16_t>(mouse_y),WINDOW_DEFAULT_H);
-        set_pixel(pos_x,pos_y, picked_type);
-        set_temp_pixel(pos_x, pos_y, picked_type);
+        set_pixel(pos_x,pos_y, picked_type,false);
     }
 
     for (uint16_t x = 0; x<WINDOW_DEFAULT_W; x++){
     for (uint16_t y = 0; y<WINDOW_DEFAULT_H; y++){
+        if (get_placed_mask(x,y)) continue;
         switch (get_pixel(x,y)){
         case P_SAND:
-            if (liquids[P_DOWN]){
-                set_temp_pixel(x,y,P_DOWN);
-                set_temp_pixel(x,y-1,P_SAND);
+            if (liquids[get_pixel(P_DOWN)] && !get_placed_mask(P_DOWN)){
+                set_pixel(x,y,get_pixel(P_DOWN));
+                set_pixel(x,y-1,P_SAND);
             }
             else if (y>1){
-                if (liquids[P_DOWN_LEFT] && x>0 ){
-                    set_temp_pixel(x,y,P_DOWN_LEFT);
-                    set_temp_pixel(x-1,y-1,P_SAND);
+                if (liquids[get_pixel(P_DOWN_LEFT)] && x>0 && !get_placed_mask(P_DOWN_LEFT)){
+                    set_pixel(x,y,get_pixel(P_DOWN_LEFT));
+                    set_pixel(x-1,y-1,P_SAND);
                 }
-                else if (liquids[P_DOWN_RIGHT] && x < WINDOW_DEFAULT_W){
-                    set_temp_pixel(x,y,P_DOWN_RIGHT);
-                    set_temp_pixel(x+1,y-1,P_SAND);
+                else if (liquids[get_pixel(P_DOWN_RIGHT)] && x < WINDOW_DEFAULT_W && !get_placed_mask(P_DOWN_RIGHT)){
+                    set_pixel(x,y,get_pixel(P_DOWN_RIGHT));
+                    set_pixel(x+1,y-1,P_SAND);
                 }
             }
             break;
         case P_WATER:
-            if (gases[P_DOWN]){
-                set_temp_pixel(x,y,P_DOWN);
-                set_temp_pixel(x,y-1,P_WATER);
+            if (gases[get_pixel(P_DOWN)]&& !get_placed_mask(P_DOWN)){
+                set_pixel(x,y,get_pixel(P_DOWN));
+                set_pixel(x,y-1,P_WATER);
             }
-            /*else if (y>1){
-                if (gases[P_LEFT] && x>0){
-                    set_temp_pixel(x,y,P_LEFT);
-                    set_temp_pixel(x-1,y,P_WATER);
+            else if (y>1){
+                if (gases[get_pixel(P_LEFT)] && x>0 && !get_placed_mask(P_LEFT) && random_50_50()){
+                    set_pixel(x,y,get_pixel(P_LEFT));
+                    set_pixel(x-1,y,P_WATER);
                 }
-                else if (gases[P_RIGHT] && x < WINDOW_DEFAULT_W){
-                    set_temp_pixel(x,y,P_RIGHT);
-                    set_temp_pixel(x+1,y,P_WATER);
+                else if (gases[get_pixel(P_RIGHT)] && x < WINDOW_DEFAULT_W && !get_placed_mask(P_LEFT) && random_50_50()){
+                    set_pixel(x,y,get_pixel(P_RIGHT));
+                    set_pixel(x+1,y,P_WATER);
                 }
             }
-            */
+            
         }
     }
     }
-    // Copy temp pixels into real ones and reset them
-    memcpy(&pixels, &temp_pixels, sizeof(temp_pixels));
+    // Reset Placed Mask
+    memset(placed_mask,false,sizeof(placed_mask));
 }
 
 //                         void   sand     water   invalid
@@ -158,7 +157,6 @@ int main() {
     
     // Initialize arrays
     for (int i = 0; i<P_COUNT; i++){
-        temp_pixels[i] = P_VOID;
         pixels[i] = P_VOID;
     }
 
