9acd2fe4e3e3abe93491eeee940446750f14a21a
TotallyKeithYes <135842382+GopherDevGames@users.noreply.github.com>
2024-05-13 00:57:15 +0200

    initial water tests

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+#include "SDL_scancode.h"
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <cstdint>
@@ -6,22 +6,22 @@
 #include <string>
 #include <chrono>
 
-const uint16_t size_multiplier = 2;
-
-const uint16_t WINDOW_DEFAULT_W = 1280;
-const uint16_t WINDOW_DEFAULT_H = 720;
-
-const uint32_t P_COUNT = (WINDOW_DEFAULT_H*WINDOW_DEFAULT_W)/size_multiplier;
+const uint16_t size_multiplier = 4;
+const uint16_t WINDOW_DEFAULT_W = (1280)/size_multiplier;
+const uint16_t WINDOW_DEFAULT_H = (720)/size_multiplier;
+const uint32_t P_COUNT = (WINDOW_DEFAULT_H*WINDOW_DEFAULT_W);
 
 const uint8_t P_VOID = 0;
 const uint8_t P_SAND = 1;
 const uint8_t P_WATER = 2;
 const uint8_t P_TYPES_AMOUNT = 3;
+                                        //void       sand      water
+const uint8_t gases[P_TYPES_AMOUNT] = {true,false,false};
+const uint8_t liquids[P_TYPES_AMOUNT] = {true,false,true};
+const uint8_t solids[P_TYPES_AMOUNT] = {false,true,false};
 
-uint8_t temp_pixels[WINDOW_DEFAULT_H*WINDOW_DEFAULT_W];
-uint8_t pixels[WINDOW_DEFAULT_H*WINDOW_DEFAULT_W];
-
-const auto framerate = 1.0/144.0;
+uint8_t temp_pixels[P_COUNT];
+uint8_t pixels[P_COUNT];
 
 uint8_t get_pixel(uint16_t x,uint16_t y){
     return pixels[(x*WINDOW_DEFAULT_H)+y];
@@ -38,42 +38,69 @@ void set_pixel(uint16_t x,uint16_t y,uint8_t pixel){
 }
 
 #define P_DOWN get_temp_pixel(x, static_cast<uint16_t>(std::max(y-1,1)))
+#define P_LEFT get_temp_pixel(static_cast<uint16_t>(std::max(x-1,1)), y)
+#define P_RIGHT get_temp_pixel(static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), y)
 #define P_DOWN_LEFT get_temp_pixel(static_cast<uint16_t>(std::max(x-1,1)), static_cast<uint16_t>(std::max(y-1,1)))
 #define P_DOWN_RIGHT get_temp_pixel(static_cast<uint16_t>(std::min(x+1,WINDOW_DEFAULT_W-1)), static_cast<uint16_t>(std::max(y-1,1)))
 
 // GLobals
 bool is_drawing = false;
+auto framerate = 60.0;
+uint8_t picked_type = P_SAND;
+
+bool random_50_50() {
+   return rand() > (RAND_MAX / 2);
+}
+
 
 void run_logic(){
 
     if (is_drawing){
-        int x, y;
-        SDL_GetMouseState(&x, &y);
-        y = WINDOW_DEFAULT_H-y;
-        uint16_t pos_x = std::min(static_cast<uint16_t>(x),WINDOW_DEFAULT_W);
-        uint16_t pos_y = std::min(static_cast<uint16_t>(y),WINDOW_DEFAULT_H);
-        set_pixel(pos_x,pos_y, P_SAND);
+        int mouse_x, mouse_y;
+        SDL_GetMouseState(&mouse_x, &mouse_y);
+        mouse_y = WINDOW_DEFAULT_H-(mouse_y/size_multiplier);
+        mouse_x/=size_multiplier;
+        uint16_t pos_x = std::min(static_cast<uint16_t>(mouse_x),WINDOW_DEFAULT_W);
+        uint16_t pos_y = std::min(static_cast<uint16_t>(mouse_y),WINDOW_DEFAULT_H);
+        set_pixel(pos_x,pos_y, picked_type);
+        set_temp_pixel(pos_x, pos_y, picked_type);
     }
 
     for (uint16_t x = 0; x<WINDOW_DEFAULT_W; x++){
     for (uint16_t y = 0; y<WINDOW_DEFAULT_H; y++){
         switch (get_pixel(x,y)){
         case P_SAND:
-            if (P_DOWN == P_VOID){
-                set_temp_pixel(x,y,P_VOID);
+            if (liquids[P_DOWN]){
+                set_temp_pixel(x,y,P_DOWN);
                 set_temp_pixel(x,y-1,P_SAND);
             }
             else if (y>1){
-                if (P_DOWN_LEFT == P_VOID){
-                    set_temp_pixel(x,y,P_VOID);
+                if (liquids[P_DOWN_LEFT] && x>0 ){
+                    set_temp_pixel(x,y,P_DOWN_LEFT);
                     set_temp_pixel(x-1,y-1,P_SAND);
                 }
-                else if (P_DOWN_RIGHT == P_VOID){
-                    set_temp_pixel(x,y,P_VOID);
+                else if (liquids[P_DOWN_RIGHT] && x < WINDOW_DEFAULT_W){
+                    set_temp_pixel(x,y,P_DOWN_RIGHT);
                     set_temp_pixel(x+1,y-1,P_SAND);
                 }
             }
             break;
+        case P_WATER:
+            if (gases[P_DOWN]){
+                set_temp_pixel(x,y,P_DOWN);
+                set_temp_pixel(x,y-1,P_WATER);
+            }
+            /*else if (y>1){
+                if (gases[P_LEFT] && x>0){
+                    set_temp_pixel(x,y,P_LEFT);
+                    set_temp_pixel(x-1,y,P_WATER);
+                }
+                else if (gases[P_RIGHT] && x < WINDOW_DEFAULT_W){
+                    set_temp_pixel(x,y,P_RIGHT);
+                    set_temp_pixel(x+1,y,P_WATER);
+                }
+            }
+            */
         }
     }
     }
@@ -92,15 +119,27 @@ void render(SDL_Renderer *rend){
     for (int x = 0; x<WINDOW_DEFAULT_W; x++){
     for (int y = 0; y<WINDOW_DEFAULT_H; y++){
         auto pixel = get_pixel(static_cast<uint16_t>(x), static_cast<uint16_t>(y));
-        if (pixel == P_VOID) continue;
+        if (pixel == P_VOID) continue; // Don't render empty pixels, bonus optimization
+        SDL_Point points[size_multiplier*size_multiplier];
+        for (int size_w = 0; size_w < size_multiplier; size_w++){
+        for (int size_h = 0; size_h < size_multiplier; size_h++){
+            points[(size_w*size_multiplier)+size_h] = SDL_Point{(x*size_multiplier)+size_w,((WINDOW_DEFAULT_H-y)*size_multiplier)+size_h};
+        }
+        }
         SDL_SetRenderDrawColor(rend, colors_r[pixel], colors_g[pixel], colors_b[pixel], 255);
-        SDL_RenderDrawPoint(rend, x, WINDOW_DEFAULT_H-y);
+        SDL_RenderDrawPoints(rend, points, size_multiplier*size_multiplier);
     }
     }
     SDL_RenderPresent(rend);
 }
 
 int main() {
+
+    std::cout<<"Enter simulation Framerate (default:60): "<<std::endl;
+    if (std::cin.peek() == '\n' || !(std::cin>>framerate)){
+        framerate = 60.0;
+    }
+
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
         printf("Game will now self destruct.");
@@ -109,7 +148,7 @@ int main() {
     SDL_Window *win =
         SDL_CreateWindow("CppSand", // creates a window
                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       WINDOW_DEFAULT_W, WINDOW_DEFAULT_H, 0);
+                       WINDOW_DEFAULT_W*size_multiplier, WINDOW_DEFAULT_H*size_multiplier, 0);
 
     SDL_Renderer *rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     SDL_RenderSetVSync(rend, 0);
@@ -118,7 +157,7 @@ int main() {
     auto last_tick = std::chrono::high_resolution_clock::now();
     
     // Initialize arrays
-    for (int i = 0; i<WINDOW_DEFAULT_H*WINDOW_DEFAULT_W; i++){
+    for (int i = 0; i<P_COUNT; i++){
         temp_pixels[i] = P_VOID;
         pixels[i] = P_VOID;
     }
@@ -159,9 +198,14 @@ int main() {
 
             case SDL_KEYDOWN:
                 switch (event.key.keysym.scancode) {
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    break;
+                case SDL_SCANCODE_1:
+                case SDL_SCANCODE_KP_1:
+                picked_type = P_SAND;
+                break;
+                case SDL_SCANCODE_2:
+                case SDL_SCANCODE_KP_2:
+                picked_type = P_WATER;
+                break;
                 }
             }
         }
@@ -169,8 +213,10 @@ int main() {
         render(rend);
 
         printf("%s", SDL_GetError());
-
-        //SDL_Delay(framerate-delta);
+        double delay = ((1.0/framerate)-delta)*1000.0;
+        if (delay < 0) delay = 0.0;
+        if (delay > 1000.0) delay = 1000.0;
+        SDL_Delay(static_cast<uint32_t>(delay));
         last_tick = current_tick;
     }
 
